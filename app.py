59713b77d77f813a40b6abdecee2c7fee39b6056
MohamedMahfouzzzzz <168643355+MohamedMahfouzzzzz@users.noreply.github.com>
2025-05-19 20:50:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def download():
         'quiet': True,
         'outtmpl': os.path.join(tempfile.gettempdir(), '%(title)s.%(ext)s'),
         'no_warnings': True,
+        'cookiefile': 'www.youtube.com_cookies.txt',
         'ignoreerrors': True,
         'headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
